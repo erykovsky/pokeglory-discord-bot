c9e3cf6087a0ff9fb9ffdd1c401dd421a3015d02
erykovsky <jageryk19@gmail.com>
2024-10-30 10:44:02 +0100

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import "dotenv/config";
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import { config } from "./config";
 
-const client = new Client({
-  intents: [
-    "Guilds",
-    "GuildMessages",
-    "DirectMessages",
-    "GuildMembers",
-    "MessageContent",
-  ],
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on("ready", () => {
+  console.log("Discord bot is ready!");
 });
 
-client.on("ready", (c) => {
-  console.log(`${c.user.username} is online.`);
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("P1ong!");
+  }
 });
 
 client.login(config.DISCORD_TOKEN);
