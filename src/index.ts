3922edd9039999243c99f8792f860be7a85a0685
erykovsky <jageryk19@gmail.com>
2024-10-30 10:39:08 +0100

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,20 @@
+import "dotenv/config";
 import { Client } from "discord.js";
 import { config } from "./config";
-import { commands } from "./commands";
-import { deployCommands } from "./deploy-commands";
 
-export const client = new Client({
-  intents: ["Guilds", "GuildMessages", "DirectMessages", "MessageContent"],
+const client = new Client({
+  intents: [
+    "Guilds",
+    "GuildMessages",
+    "DirectMessages",
+    "GuildMembers",
+    "MessageContent",
+  ],
 });
 
-client.once("ready", () => {
-  console.log("Discord bot is ready! 🤖");
+client.on("ready", (c) => {
+  console.log(`${c.user.username} is online.`);
 });
-
-client.on("guildCreate", async (guild) => {
-  await deployCommands({ guildId: guild.id });
-});
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) {
-    return;
-  }
-  const { commandName } = interaction;
-  if (commands[commandName as keyof typeof commands]) {
-    commands[commandName as keyof typeof commands].execute(interaction);
-  }
-});
-
-// client.on("messageCreate", async (message: Message) => {
-//   if (
-//     message.channel.name === "bot" &&
-//     !message.author.bot &&
-//     message.author.id === "428244788618199051"
-//   ) {
-//     try {
-//       const response = await axios.post(
-//         "https://pokeglory.erykovsky.partykit.dev/parties/chat/general",
-//         {
-//           message: message.content,
-//         }
-//       );
-//       console.log(ws);
-//       // console.log("Message sent to API:", response.data);
-//     } catch (error) {
-//       console.error("Failed to send message to API:", error);
-//     }
-//   }
-// });
 
 client.login(config.DISCORD_TOKEN);
+
