151f6e8480f814e4866896b3eb939bcdbc703219
erykovsky <jageryk19@gmail.com>
2025-09-12 17:05:40 +0200

    d

--- src/index.ts
+++ src/index.ts
@@ -19,30 +19,7 @@ client.on("ready", async () => {
 
   const commands = [linkCommand, losujCommand];
 
-  // Pokaż wszystkie obecne komendy
-  const existingCommands = await client.application?.commands.fetch();
-  console.log(
-    "Obecne komendy:",
-    existingCommands?.map((cmd) => cmd.name)
-  );
-
-  // Usuń konkretnie komendę "pong" jeśli istnieje
-  const pongCommand = existingCommands?.find((cmd) => cmd.name === "ping");
-  if (pongCommand) {
-    await pongCommand.delete();
-    console.log("Komenda 'pong' usunięta");
-  }
-
-  // Usuń wszystkie komendy
-  await client.application?.commands.set([]);
-  console.log("Wszystkie komendy usunięte");
-
-  // Dodaj tylko nasze komendy
   await client.application?.commands.set(commands);
-  console.log(
-    "Komendy zaktualizowane:",
-    commands.map((cmd) => cmd.name)
-  );
 });
 
 // Handle slash commands
